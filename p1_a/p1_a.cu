2a174798cf8e09dfa34fcf63a03f522cd554c653
pablo ibarra <pibarra@localhost.localdomain>
2018-11-04 16:06:05 -0500

    p1a y p1b separadas

---
+++ p1_a/p1_a.cu
@@ -0,0 +1,131 @@
+#include <stdio.h>
+#include <math.h>
+
+void printMatrix(const int *A, int rows, int cols) {
+    for(int i = 0; i < rows*cols*4; i++){
+        printf("%d ", A[i]);   
+        printf(" ");
+        if ((i+1)%9 == 0){
+          printf("|");
+        }
+    }
+    printf("\n");
+};
+
+void readInput_aos(const char *filename, int **Aos, int *rows, int *cols) {
+  FILE *file;
+  file = fopen(filename, "r");
+  fscanf(file, "%d %d", rows, cols);
+  int * A_F1 = (int *) malloc(*rows * (*cols)* (4) * sizeof(int));
+  for(int j = 0; j <  4; j++) {
+    int counter = 0;
+    for(int i = 0; i < *cols*(*rows); i++){
+      fscanf(file, "%d ", &A_F1[counter +j]);
+      counter = counter + 4;
+      }
+  }
+  fclose(file);
+  *Aos = A_F1;
+}
+
+__global__ void step_periodic_Aos(int * array,int rows, int cols){
+  int tId = threadIdx.x + blockIdx.x * blockDim.x;
+  if (tId < rows*cols){
+    int x = tId%(cols);
+    int y = (int) tId/rows;
+
+    //Colission
+
+    if (array[tId*4+0] == 1 && array[tId*4+2] == 1){
+      if(array[tId*4+1] == 0 && array[tId*4+3] == 0){
+        array[tId*4+0] = 0;
+        array[tId*4+2] = 0;
+        array[tId*4+1] = 1;
+        array[tId*4+3] = 1;
+      }
+    }
+    if (array[tId*4+1] == 1 && array[tId*4+3] == 1){
+      if(array[tId*4+0] == 0 && array[tId*4+2] == 0){
+        array[tId*4+1] = 0;
+        array[tId*4+3] = 0;
+        array[tId*4+0] = 1;
+        array[tId*4+2] = 1;
+      }
+    }
+
+    //streaming 
+
+    //right
+    int c_aux = x + 1;
+    if (c_aux == cols){
+      c_aux = 0;
+    }
+    if (array[tId*4] == 1){
+      array[(y*rows + c_aux)*4] = array[tId*4]*2;
+    }
+
+    //left
+    c_aux = x - 1;
+    if (c_aux < 0){
+      c_aux = cols -1;
+    }
+    if (array[tId*4+2] == 1){
+      array[(y*rows + c_aux)*4 + 2] = array[tId*4+2]*2;
+    }
+
+    //top
+    c_aux = y + 1;
+    if (c_aux == rows){
+      c_aux = 0;
+    }
+    if (array[tId*4+1] == 1){
+      array[(c_aux*rows + x)*4 + 1] = array[tId*4+1]*2;
+    }
+
+    //bottom
+    c_aux = y + 1;
+    if (c_aux < 0){
+      c_aux = rows-1;
+    }
+    if (array[tId*4+3] == 1){
+      array[(c_aux*rows + x)*4 + 3] = array[tId*4+3]*2;
+    }
+
+    //Correction
+    for(int i = 0; i < 4; i++){
+      if(array[tId*4+i] == 1){
+        array[tId*4+i] = 0;
+      }
+      if(array[tId*4+i] == 2){
+        array[tId*4+i] = 1;
+      }
+    };
+  }
+};  
+
+int main(int argc, char const *argv[])
+{
+  int rows, cols;
+  int *Aos;
+  int *d_Aos;
+
+  readInput_aos("../initial.txt", &Aos, &rows, &cols);
+
+  int n = (int)(rows*cols);
+  int block_size = 256;
+  int grid_size = (int) ceil((float)n / block_size);
+
+  cudaMalloc(&d_Aos, 4 * rows * cols * sizeof(int));
+  cudaMemcpy(d_Aos, Aos, 4 * rows * cols * sizeof(int), cudaMemcpyHostToDevice);
+
+  for(int k = 0; k < 1000; k++){
+    step_periodic_Aos<<<grid_size, block_size>>>(d_Aos, rows, cols);
+  }
+
+  cudaMemcpy(Aos, d_Aos, 4 * rows * cols * sizeof(int), cudaMemcpyDeviceToHost);
+  cudaFree(d_Aos);
+
+return 0;
+
+}
+
--- p1_b/p1_b.cu
+++ p1_b/p1_b.cu
@@ -24,22 +24,6 @@ void readInput_soa(const char *filename, int **Soa,  int *rows, int *cols) {
   *Soa = A_F0;
 };
 
-void readInput_aos(const char *filename, int **Aos, int *rows, int *cols) {
-  FILE *file;
-  file = fopen(filename, "r");
-  fscanf(file, "%d %d", rows, cols);
-  int * A_F1 = (int *) malloc(*rows * (*cols)* (4) * sizeof(int));
-  for(int j = 0; j <  4; j++) {
-    int counter = 0;
-    for(int i = 0; i < *cols*(*rows); i++){
-      fscanf(file, "%d ", &A_F1[counter +j]);
-      counter = counter + 4;
-      }
-  }
-  fclose(file);
-  *Aos = A_F1;
-}
-
 __global__ void step_periodic_Soa(int * array,int rows, int cols){
   int tId = threadIdx.x + blockIdx.x * blockDim.x;
   if (tId < rows*cols){
@@ -128,91 +112,13 @@ __global__ void step_periodic_Soa(int * array,int rows, int cols){
   }
 };
 
-//Periodic boundaries condition Array of Structures
-
-__global__ void step_periodic_Aos(int * array,int rows, int cols){
-  int tId = threadIdx.x + blockIdx.x * blockDim.x;
-  if (tId < rows*cols){
-    int x = tId%(cols);
-    int y = (int) tId/rows;
-
-    //Colission
-
-    if (array[tId*4+0] == 1 && array[tId*4+2] == 1){
-      if(array[tId*4+1] == 0 && array[tId*4+3] == 0){
-        array[tId*4+0] = 0;
-        array[tId*4+2] = 0;
-        array[tId*4+1] = 1;
-        array[tId*4+3] = 1;
-      }
-    }
-    if (array[tId*4+1] == 1 && array[tId*4+3] == 1){
-      if(array[tId*4+0] == 0 && array[tId*4+2] == 0){
-        array[tId*4+1] = 0;
-        array[tId*4+3] = 0;
-        array[tId*4+0] = 1;
-        array[tId*4+2] = 1;
-      }
-    }
-
-    //streaming 
-
-    //right
-    int c_aux = x + 1;
-    if (c_aux == cols){
-      c_aux = 0;
-    }
-    if (array[tId*4] == 1){
-      array[(y*rows + c_aux)*4] = array[tId*4]*2;
-    }
-
-    //left
-    c_aux = x - 1;
-    if (c_aux < 0){
-      c_aux = cols -1;
-    }
-    if (array[tId*4+2] == 1){
-      array[(y*rows + c_aux)*4 + 2] = array[tId*4+2]*2;
-    }
-
-    //top
-    c_aux = y + 1;
-    if (c_aux == rows){
-      c_aux = 0;
-    }
-    if (array[tId*4+1] == 1){
-      array[(c_aux*rows + x)*4 + 1] = array[tId*4+1]*2;
-    }
-
-    //bottom
-    c_aux = y + 1;
-    if (c_aux < 0){
-      c_aux = rows-1;
-    }
-    if (array[tId*4+3] == 1){
-      array[(c_aux*rows + x)*4 + 3] = array[tId*4+3]*2;
-    }
-
-    //Correction
-    for(int i = 0; i < 4; i++){
-      if(array[tId*4+i] == 1){
-        array[tId*4+i] = 0;
-      }
-      if(array[tId*4+i] == 2){
-        array[tId*4+i] = 1;
-      }
-    };
-  }
-};  
-
 int main(int argc, char const *argv[])
 {
   int rows, cols;
-  int *Aos, *Soa;
-  int *d_Aos, *d_Soa;
+  int *Soa;
+  int *d_Soa;
 
-  readInput_aos("initial.txt", &Aos, &rows, &cols);
-  readInput_soa("initial.txt", &Soa, &rows, &cols);
+  readInput_soa("../initial.txt", &Soa, &rows, &cols);
 
   //printMatrix(Soa,rows,cols);
 
@@ -220,30 +126,18 @@ int main(int argc, char const *argv[])
   int block_size = 256;
   int grid_size = (int) ceil((float)n / block_size);
 
-  cudaMalloc(&d_Aos, 4 * rows * cols * sizeof(int));
   cudaMalloc(&d_Soa, 4 * rows * cols * sizeof(int));
-
-  cudaMemcpy(d_Aos, Aos, 4 * rows * cols * sizeof(int), cudaMemcpyHostToDevice);
   cudaMemcpy(d_Soa, Soa, 4 * rows * cols * sizeof(int), cudaMemcpyHostToDevice);
 
   for(int k = 0; k < 1000; k++){
-    //step_periodic_Aos<<<grid_size, block_size>>>(d_Aos, rows, cols);
     step_periodic_Soa<<<grid_size, block_size>>>(d_Soa, rows, cols);
   }
 
-  cudaMemcpy(Aos, d_Aos, 4 * rows * cols * sizeof(int), cudaMemcpyDeviceToHost);
   cudaMemcpy(Soa, d_Soa, 4 * rows * cols * sizeof(int), cudaMemcpyDeviceToHost);
-
-  cudaFree(d_Aos);
   cudaFree(d_Soa);
-  //printf("----- \n");
-  //printMatrix(Soa,rows,cols);
 
-
-  //printMatrix(Aos,rows,cols);
-  //printf("----- \n");
-  //printMatrix(Soa,rows,cols);
 
 return 0;
 
 }
+
