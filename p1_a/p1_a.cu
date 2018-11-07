9e7774bb2fd586af67dc8d5ecf06442059887667
pablodg12 <pibarra@pinguino.csrg.inf.utfsm.cl>
2018-11-06 22:52:17 -0300

    :D

--- p1_a/p1_a.cu
+++ p1_a/p1_a.cu
@@ -28,46 +28,10 @@ void readInput_aos(const char *filename, int **Aos, int *rows, int *cols) {
   *Aos = A_F1;
 }
 
-__global__ void step_periodic_Aos(int * array,int rows, int cols){
-  extern __shared__ int buffer[];
+__global__ void step_periodic(int * array,int *buffer,int rows, int cols){
   int tId = threadIdx.x + blockIdx.x * blockDim.x;
-  if(threadIdx.x < 256){
-    for(int i = threadIdx.x; i < rows*cols; i+=256 ){
-      if (array[i*4+0] == 1 && array[i*4+2] == 1){
-        if(array[i*4+1] == 0 && array[i*4+3] == 0){
-          buffer[i*4+0] = 0;
-          buffer[i*4+2] = 0;
-          buffer[i*4+1] = 1;
-          buffer[i*4+3] = 1;
-        }
-      }else if(array[i*4+1] == 1 && array[i*4+3] == 1){
-        if(array[i*4+0] == 0 && array[i*4+2] == 0){
-          buffer[i*4+1] = 0;
-          buffer[i*4+3] = 0;
-          buffer[i*4+0] = 1;
-          buffer[i*4+2] = 1;
-          }
-        }else{
-          buffer[i*4+1] = array[i*4+1];
-          buffer[i*4+3] = array[i*4+3];
-          buffer[i*4+0] = array[i*4+0];
-          buffer[i*4+2] = array[i*4+2];
-      }
-    }
-  }
-  __syncthreads();
 
-  //if(tId == 1){
-  //  for(int i = 0; i < rows*cols*4;i++){
-  //    printf("%d ", buffer[i]);
-  //    printf(" ");
-  //  if ((i+1)%4 == 0){
-  //        printf("|");
-  //    }
- // }
-  //printf("\n");
-//}
-  if (tId < rows*cols){
+if(tId < rows*cols){
     int x = tId%(cols);
     int y = (int) tId/rows;
 
@@ -75,8 +39,12 @@ __global__ void step_periodic_Aos(int * array,int rows, int cols){
     if (c_aux < 0){
       c_aux = cols -1;
     }
-    if (buffer[(y*rows + c_aux)*4] == 1){
-      array[tId*4] = 1;
+    if (buffer[(y*rows + c_aux)*4 + 1] == 1 && buffer[(y*rows + c_aux)*4 + 3] == 1){
+       array[tId*4] = 1;
+    }else if (buffer[(y*rows + c_aux)*4] == 1 && buffer[(y*rows + c_aux) + 2] == 1){
+      array[tId*4] = 0;
+    }else if (buffer[(y*rows + c_aux)*4] == 1){
+        array[tId*4] = 1;
     }else if (buffer[(y*rows + c_aux)*4] == 0){
       array[tId*4] = 0;
     }
@@ -85,31 +53,38 @@ __global__ void step_periodic_Aos(int * array,int rows, int cols){
     if (c_aux == cols){
       c_aux = 0;
     }
-    if (buffer[(y*rows + c_aux)*4+2] == 1){
-      array[tId*4+2] = 1;
-    }else if (buffer[(y*rows + c_aux)*4+2] == 0){
+
+    if (buffer[(y*rows + c_aux)*4+1] == 1 && buffer[(y*rows + c_aux)*4 + 3] == 1){
+       array[tId*4+2] = 1;
+    }else if (buffer[(y*rows + c_aux)*4] == 1 && buffer[(y*rows + c_aux)*4 + 2] == 1){
+      array[tId*4+2] = 0;
+    }else if (buffer[(y*rows + c_aux)*4+ 2] == 1){
+        array[tId*4+2] = 1;
+    }else if (buffer[(y*rows + c_aux)*4+ 2] == 0){
       array[tId*4+2] = 0;
     }
 
-    //top
-    c_aux = y - 1;
-    if (c_aux <0){
-      c_aux = rows-1;
-    }
-    if (buffer[(c_aux*rows + x)*4 + 1] == 1){
+    c_aux = (((y-1)%rows)+rows)%rows*cols;
+
+    if (buffer[(c_aux + x)*4] == 1 && buffer[(c_aux + x)*4+2] == 1){
+       array[tId*4+1] = 1;
+    }else if (buffer[(c_aux + x)*4+1] == 1 && buffer[(c_aux + x)*4+3] == 1){
+      array[tId*4+1] = 0;
+    }else if (buffer[ (c_aux + x)*4 + 1 ] == 1){
       array[tId*4+1] = 1;
-    }else if (buffer[(c_aux*rows + x)*4+1] == 0){
+    }else if (buffer[ (c_aux + x)*4 + 1 ] == 0){
       array[tId*4+1] = 0;
     }
 
-    //bottom
-    c_aux = y + 1;
-    if (c_aux == rows){
-      c_aux = 0;
-    }
-    if (buffer[(c_aux*rows + x)*4 + 3] == 1){
+    c_aux = (((y+1)%rows)*cols);
+
+    if (buffer[(c_aux + x)*4] == 1 && buffer[(c_aux + x)*4 + 2] == 1){
+       array[tId*4+3] = 1;
+    }else if (buffer[(c_aux + x)*4+1] == 1 && buffer[(c_aux + x)*4 + 3] == 1){
+      array[tId*4+3] = 0;
+    }else if (buffer[ (c_aux + x)*4 + 3 ] == 1){
       array[tId*4+3] = 1;
-    }else if(buffer[(c_aux*rows + x)*4+3] == 0){
+    }else if (buffer[ (c_aux + x)*4 +3] == 0){
       array[tId*4+3] = 0;
     }
   }
@@ -118,25 +93,26 @@ __global__ void step_periodic_Aos(int * array,int rows, int cols){
 int main(int argc, char const *argv[])
 {
   int rows, cols;
-  int *Aos;
-  int *d_Aos;
-
-  readInput_aos("../initial.txt", &Aos, &rows, &cols);
+  int *array;
+  int *d_array;
+  int *d_buffer;
+  readInput_aos("../initial.txt", &array, &rows, &cols);
 
   int n = (int)(rows*cols);
   int block_size = 256;
-  int grid_size = (int) ceil((float)n / block_size);
+  int grid_size = (int) ceil((float) n/ block_size);
 
-  cudaMalloc(&d_Aos, 4 * rows * cols * sizeof(int));
-  cudaMemcpy(d_Aos, Aos, 4 * rows * cols * sizeof(int), cudaMemcpyHostToDevice);
-
+  cudaMalloc(&d_array ,4*rows * cols * sizeof(int));
+  cudaMalloc(&d_buffer,4*rows*cols*sizeof(int));
+  cudaMemcpy(d_array, array,4* rows * cols * sizeof(int), cudaMemcpyHostToDevice);
+  cudaMemcpy(d_buffer, array,4* rows * cols * sizeof(int), cudaMemcpyHostToDevice);
   for(int k = 0; k < 1000; k++){
-    step_periodic_Aos<<<grid_size, block_size,rows*cols*4>>>(d_Aos, rows, cols);
+    step_periodic<<<grid_size, block_size>>>(d_array, d_buffer, rows, cols);
+    cudaMemcpy(d_buffer,d_array,4*rows*cols * sizeof(int), cudaMemcpyDeviceToDevice);
   }
+  cudaMemcpy(array, d_array, 4*rows * cols * sizeof(int), cudaMemcpyDeviceToHost);
+  cudaFree(d_array);
+  cudaFree(d_buffer);
 
-  cudaMemcpy(Aos, d_Aos, 4 * rows * cols * sizeof(int), cudaMemcpyDeviceToHost);
-  cudaFree(d_Aos);
-
-return 0;
-
+  return(0);
 }
