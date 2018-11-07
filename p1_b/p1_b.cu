18505eb3e37ab249c84e91e97b229794d6ff5717
pablodg12 <pibarra@pinguino.csrg.inf.utfsm.cl>
2018-11-06 22:34:01 -0300

    work

--- p1_b/p1_b.cu
+++ p1_b/p1_b.cu
@@ -50,13 +50,13 @@ if(tId < rows*cols){
     }
 
     if (buffer[(y*rows + c_aux) + rows*cols] == 1 && buffer[(y*rows + c_aux) + 3*rows*cols] == 1){
-       array[tId] = 1;
+       array[tId+2*rows*cols] = 1;
     }else if (buffer[(y*rows + c_aux)] == 1 && buffer[(y*rows + c_aux) + 2*rows*cols] == 1){
-      array[tId] = 0;
+      array[tId+2*rows*cols] = 0;
     }else if (buffer[(y*rows + c_aux)+ 2*rows*cols] == 1){
-        array[tId] = 1;
+        array[tId+2*rows*cols] = 1;
     }else if (buffer[(y*rows + c_aux)+ 2*rows*cols] == 0){
-      array[tId] = 0;
+      array[tId+2*rows*cols] = 0;
     }
     c_aux = y - 1;
     if (c_aux <0){
@@ -66,58 +66,46 @@ if(tId < rows*cols){
     c_aux = (((y-1)%rows)+rows)%rows*cols;
 
     if (buffer[(c_aux + x)] == 1 && buffer[(c_aux + x) + 2*rows*cols] == 1){
-       array[tId] = 1;
+       array[tId+rows*cols] = 1;
     }else if (buffer[(c_aux + x)+rows*cols] == 1 && buffer[(c_aux + x) + 3*rows*cols] == 1){
-      array[tId] = 0;
+      array[tId+rows*cols] = 0;
     }else if (buffer[ (c_aux + x) + rows*cols ] == 1){
-      array[tId + rows*cols] = 1;
+      array[tId+rows*cols] = 1;
     }else if (buffer[ (c_aux + x) + rows*cols ] == 0){
-      array[tId + rows*cols] = 0;
+      array[tId+rows*cols] = 0;
     }
 
     c_aux = (((y+1)%rows)*cols);
 
     if (buffer[(c_aux + x)] == 1 && buffer[(c_aux + x) + 2*rows*cols] == 1){
-       array[tId] = 1;
+       array[tId+3*rows*cols] = 1;
     }else if (buffer[(c_aux + x)+rows*cols] == 1 && buffer[(c_aux + x) + 3*rows*cols] == 1){
-      array[tId] = 0;
+      array[tId+3*rows*cols] = 0;
     }else if (buffer[ (c_aux + x) + 3*rows*cols ] == 1){
-      array[tId + rows*cols] = 1;
+      array[tId+3*rows*cols] = 1;
     }else if (buffer[ (c_aux + x) + 3*rows*cols ] == 0){
-      array[tId + rows*cols] = 0;
+      array[tId+3*rows*cols] = 0;
     }
   }
+   if(tId == 1){
+    for(int i = 0; i < 4*rows*cols;i++){
+      printf("%d ", array[i]);
+      
+        if ((i+1)%9 == 0){
+          printf("|");
+        }
 }
-/*
-    c_aux = y - 1;
-    if (c_aux <0){
-      c_aux = rows-1;
-    }
-    if (buffer[ (c_aux*rows + x) + rows*cols ] == 1){
-      array[tId + rows*cols] = 1;
-    }else if (buffer[ (c_aux*rows + x) + rows*cols ] == 0){
-      array[tId + rows*cols] = 0;
-    }
-    c_aux = y + 1;
-    if (c_aux == rows){
-      c_aux = 0;
-    }
-    if (buffer[ (c_aux*rows + x)+ 3*rows*cols ] == 1){
-      array[tId+ 3*rows*cols] = 1;
-    }else if (buffer[ (c_aux*rows + x) + 3*rows*cols ] == 0){
-      array[tId + 3*rows*cols] = 0;
-    }
-    */
-
-
+    printf("\n");
+ }
+ }  
 int main(int argc, char const *argv[])
 {
   int rows, cols;
   int *array;
   int *d_array;
   int *d_buffer;
-  readInput_soa("../initial.txt", &array, &rows, &cols);
-
+  readInput_soa("../initial4.txt", &array, &rows, &cols);
+  printMatrix(array,rows,cols);
   int n = (int)(rows*cols);
   int block_size = 256;
   int grid_size = (int) ceil((float) n/ block_size);
@@ -126,7 +114,7 @@ int main(int argc, char const *argv[])
   cudaMalloc(&d_buffer,4*rows*cols*sizeof(int));
   cudaMemcpy(d_array, array,4* rows * cols * sizeof(int), cudaMemcpyHostToDevice);
   cudaMemcpy(d_buffer, array,4* rows * cols * sizeof(int), cudaMemcpyHostToDevice);
-  for(int k = 0; k < 1000; k++){
+  for(int k = 0; k < 10; k++){
     step_periodic<<<grid_size, block_size>>>(d_array, d_buffer, rows, cols);
     cudaMemcpy(d_buffer,d_array,4*rows*cols * sizeof(int), cudaMemcpyDeviceToDevice);
   }
